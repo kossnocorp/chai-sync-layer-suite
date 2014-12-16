3d6123ec13ad4c7cfb85a5315a5427fc172603e1
Sasha Koss <koss@nocorp.me>
2014-12-17 01:55:50 +0530

    Hello cruel world!

---
+++ src/__tests__/_test_helper.js
@@ -0,0 +1,22 @@
+
+// Chai setup
+
+var chai = require('chai');
+global.expect = chai.expect;
+
+chai.use(require('chai-shallow-deep-equal'));
+
+// Sinon
+
+chai.use(require('sinon-chai'));
+
+global.sinon = require('sinon');
+
+// Rewire
+
+global.rewire = require('rewire');
+
+var rewireTestHelpers = require('rewire-test-helpers');
+global.injectDependencies = rewireTestHelpers.injectDependencies;
+global.rewired = rewireTestHelpers.rewired;
+
